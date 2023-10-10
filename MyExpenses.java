44e515f744fadc723757063bf0c58e3d83930879
delaneybaker <u694427660@byui.edu>
2023-10-10 17:50:52 -0500

    first commit

---
+++ MyExpenses.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class MyExpenses {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your monthly budget: ");
+        double monthlyBudget = scanner.nextDouble();
+
+        System.out.println("Enter the number of categories you want to track: ");
+        int numberOfCategories = scanner.nextInt();
+
+        double totalSpending = 0;
+
+        for (int i = 0; i < numberOfCategories; i++) {
+            System.out.println("Enter the spending for category " + (i + 1) + ": ");
+            double spending = scanner.nextDouble();
+            totalSpending += spending;
+        }
+
+        if (totalSpending <= monthlyBudget) {
+            System.out.println("Congratulations! You are under budget.");
+        } else {
+            System.out.println("Unfortunately, you are over budget.");
+        }
+        scanner.close();
+    }
+}
